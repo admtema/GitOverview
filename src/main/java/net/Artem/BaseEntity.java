dc81cc6cb4bd48ec643e85c03a0feee620045c2e
Artem Molodtsov <admterentos@gmail.com>
2022-02-15 22:34:00 +0300

    adding class Person.java

--- src/main/java/net/Artem/BaseEntity.java
+++ src/main/java/net/Artem/BaseEntity.java
@@ -1,2 +1,26 @@
-package net.Artem;public class BaseEntity {
+package net.Artem;
+
+public class BaseEntity {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "id=" + id +
+                '}';
+    }
+    public boolean isNew () {
+        return (this.id == null);
+    }
 }
+
+
